aa048ff6197767713e1e6f2ba2f10bd9600498eb
MinJeong Hong <147925385+mindaaaa@users.noreply.github.com>
2025-05-19 14:14:19 +0900

    feat: 스테이징 영역(index) 업데이트 addFileToIndex 구현

--- src/core/addFileToIndex.js
+++ src/core/addFileToIndex.js
@@ -1,3 +1,20 @@
-function addFileToIndex(hash) {}
+const fs = require('fs');
+const path = require('path');
+
+const GIT_DIR = path.resolve('.mini-git');
+const INDEX_PATH = path.join(GIT_DIR, 'index.json');
+
+// {
+//   "hello.txt": "e69de2..."
+// }
+function addFileToIndex(fileName, hash) {
+  const index = fs.existsSync(INDEX_PATH)
+    ? JSON.parse(fs.readFileSync(INDEX_PATH), 'utf-8')
+    : {};
+
+  index[fileName] = hash;
+
+  fs.writeFileSync(INDEX_PATH, JSON.stringify(index, null, 2));
+}
 
 module.exports = addFileToIndex;
