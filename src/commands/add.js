007f218f5cc79db220c14b76c55e1036a84f6209
MinJeong Hong <147925385+mindaaaa@users.noreply.github.com>
2025-05-19 14:29:01 +0900

    feat: git add 명령어 구현

--- src/commands/add.js
+++ src/commands/add.js
@@ -1,6 +1,22 @@
+const fs = require('fs');
+const path = require('path');
 const createBlobObject = require('../core/createBlobObject');
 const addFileToIndex = require('../core/addFileToIndex');
 
-function add(content) {}
+function add(filename) {
+  const filePath = path.resolve(filename);
+  if (fs.existsSync(filePath)) {
+    console.log(
+      `fatal:  '${filename}'경로명세가 어떤 파일과도 일치하지 않습니다`
+    );
+    return;
+  }
+
+  const content = fs.readFileSync(filePath, 'utf-8');
+  const hash = createBlobObject(content);
+
+  addFileToIndex(filename, hash);
+  console.log(`${filename}이 스테이징 되었습니다. (${hash})`);
+}
 
 module.exports = add;
