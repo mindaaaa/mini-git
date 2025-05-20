d67ceba27671d8857c917e5d48e0e175117608ba
MinJeong Hong <147925385+mindaaaa@users.noreply.github.com>
2025-05-20 19:52:09 +0900

    feat: index.js에 commit 설정 추가

--- src/core/createTreeHash.js
+++ src/core/createTreeHash.js
@@ -1,28 +1,17 @@
-const fs = require('fs');
 const path = require('path');
-const crypto = require('crypto');
 const writeObject = require('./writeObject');
 
 const GIT_DIR = path.resolve('.mini-git');
 
-// index: { "file1.txt": "abcd1234...", "file2.txt": "efgh5678..." }
-function createTreeObject(index) {
-  // Object.entries(index) => [["file1.txt", "abcd1234..."], ["file2.txt", "efgh5678..."]]
+function createTreeHash(index) {
   const fileEntries = Object.entries(index).map(([filename, hash]) => {
     return `100644 ${filename}\\0${hash}`;
   });
 
-  // fileEntries = [
-  //   "100644 file1.txt\\0abcd1234...",
-  //   "100644 file2.txt\\0efgh5678..."
-  // ]
   const treeContent = fileEntries.join('\n');
-  // 100644 file1.txt\0abcd1234...
-  // 100644 file2.txt\0efgh5678...
 
   const buffer = Buffer.from(treeContent, 'utf-8');
   const treeHeader = `tree ${buffer.length}\\0`;
-  // "tree 123\\0" (예시로 123은 실제 길이)
 
   const store = Buffer.concat([Buffer.from(treeHeader), buffer]);
   // store = "tree 123\\0100644 file1.txt\\0abcd1234...\n100644 file2.txt\\0efgh5678..."
@@ -30,4 +19,4 @@ function createTreeObject(index) {
   return writeObject(store);
 }
 
-module.exports = createTreeObject;
+module.exports = createTreeHash;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,26 @@
 const init = require('../src/commands/init');
 const add = require('../src/commands/add');
+const commit = require('../src/commands/commit');
 
-const [, , command, filename] = process.argv;
+const [, , command, ...args] = process.argv;
 
 if (command === 'init') {
   init();
 }
 
 if (command === 'add') {
+  const filename = args[0];
   if (!filename) {
     console.log('아무 것도 지정하지 않았으므로 아무 것도 추가하지 않습니다.');
   }
   add(filename, { basePath: '.mini-git' });
+}
+
+if (command === 'commit') {
+  const message = args.join(' ').trim();
+  if (!message) {
+    console.error('⚠️ 커밋 메시지를 입력해주세요.');
+    return;
+  }
+  commit(message);
 }
